0afa3818774ac2a1c9057891d151dc670241aa19
Joshua Alvarado <joshalva3@gmail.com>
2023-08-07 23:38:19 -0500

    scoping in practice a few additonal notes in the doc

--- script.js
+++ script.js
@@ -3,4 +3,26 @@
 // google doc to notes on this section
 // https://docs.google.com/document/d/1W6gmOdr6xNLVweD0uXPaxlubdxQENUQRXewkJJhdK2w/edit
 
-// updating notes commit of the day
+// defined in a global scope
+// top level scope
+// function creates its own scope within the brackets
+// function scope
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+
+  function printAge() {
+    const output = `you are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+// global variable
+// which means it is available inside the function
+// variable lookup
+const firstName = 'Joshua';
+calcAge(1993);
